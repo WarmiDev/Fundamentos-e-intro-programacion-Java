21e67dd19c1f8b0c41d60a6c541ec3de3c0e6c62
WarmiDev <luciaguamangavilano@gmail.com>
2020-09-06 15:37:42 -0400

    correcciones a la unid3

--- ejerciciosUnidad3/CopiaSeguridad.java
+++ ejerciciosUnidad3/CopiaSeguridad.java
@@ -1,9 +1,11 @@
 public class CopiaSeguridad{
-	public int calcularCantCds(){
-		int infoTotalGb=5;
-		int tamCdEnMb=700;
-		int infoTotalMb=infoTotalGb*1024;
-		int res=infoTotalMb/700+1;
-		return res;
-	}
+    public int calcularCantCds(){
+        int infoTotalGb=5;
+        int tamCdEnMb=700;
+        int infoTotalMb=infoTotalGb*1024;
+        int res=infoTotalMb/700;
+        if(infoTotalMb/700!=0)
+            res++;
+        return res;
+    }
 }
--- ejerciciosUnidad3/ReglasHogar.java
+++ ejerciciosUnidad3/ReglasHogar.java
@@ -5,10 +5,8 @@ public class ReglasHogar
         int diaSem=3; //lunes: 1, martes: 2, etc
         do{
             contDia++;
-            if(diaSem==7)
-                diaSem=1;
-            else 
-                diaSem++;
+            diaSem++;
+            diaSem%=7;
         } while((diaSem!=3)||(contDia%2!=0));
         return contDia;
     }
